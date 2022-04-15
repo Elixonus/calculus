a7824fbc8bc971196b110441b4ae5de2e768263e
Daxonus <elias@dobrin.net>
2022-04-15 10:56:58 -0700

    added trapezoid

--- numerical_integration.py
+++ numerical_integration.py
@@ -2,21 +2,35 @@ from math import sin
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 1, 100)
+number_steps = 45
+end_x = 30
+
+x = np.linspace(0, end_x, 10000)
 y = np.sin(x)
 y_derivative = np.cos(x)
 # integral of y(x) is -cos(x)
-y_integral = -np.cos(x)
+y_integral = -np.cos(x) + 1
 
 
-number_steps = 100
-y_integral_euler = np.empty(100)
+x_numerical = np.linspace(0, end_x, number_steps)
+
+y_integral_euler = np.empty(number_steps)
 last = 0
 for n in range(number_steps):
     y_integral_euler[n] = last
-    last += 0.01 * y[n]
+    last += sin(x_numerical[n]) * (end_x / number_steps)
+
+y_integral_trapezoid = np.empty(number_steps)
+
+last = 0
+for n in range(number_steps - 1):
+    y_integral_trapezoid[n] = last
+    last += 0.5 * (sin(x_numerical[n]) + sin(x_numerical[n + 1])) * (end_x / number_steps)
+y_integral_trapezoid[-1] = last
+
 
 fig, ax = plt.subplots()
-ax.plot(x, y_integral_euler)
 ax.plot(x, y_integral)
+ax.plot(x_numerical, y_integral_euler)
+ax.plot(x_numerical, y_integral_trapezoid)
 plt.show()
